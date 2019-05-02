eb327a75634a8ea096d96effef37df6a5396223a
Vishnu Chennuru <vishnuchen@gmail.com>
2019-05-02 17:08:47 +0000

    Call back anonimous

---
+++ Callbacks/callbackAno.js
@@ -0,0 +1,13 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+var result = input.map(function(pyt) {
+
+     return Math.sqrt(Math.pow((pyt.x), 2) + Math.pow((pyt.y), 2));
+    });
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
