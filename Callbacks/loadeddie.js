7b20470b6a869c119e348e10afee277d6608e90a
Vishnu Chennuru <vishnuchen@gmail.com>
2019-05-02 19:44:29 +0000

    fixed gambling

---
+++ Callbacks/loadeddie.js
@@ -0,0 +1,14 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  var counter = -1;
+  return function() {
+    counter += 1;
+    return list[counter];
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
